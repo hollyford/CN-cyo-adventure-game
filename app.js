8483fdfbb87840aa8863e8cb0a17be6db1cb2429
Holly Ford <holly_ford@hotmail.com>
2021-11-20 12:28:50 +0000

    decided on route to take with code

--- app.js
+++ app.js
@@ -1,54 +1,55 @@
-// Standard page variables
-const headerText = document.getElementById("headerText")
-const mainText = document.getElementById("mainText")
-const mainImg = document.getElementById("mainImg")
-let goBttn = document.getElementById("goBttn")
+let currentScene = 0;
 
-// buttons
-const bttn2 = document.getElementById("bttn12")
+const replaceNodeText = (id, newText) => {
+    let node = document.getElementById(id);
+    while (node.firstChild)
+        node.removeChild(node.firstChild);
+    node.appendChild(document.createTextNode(newText));
+}
 
-// Game code begins
-// first page/choice
-headerText.textContent = "Welcome to the game"
-mainText.textContent = "Hit start to go on an adventure!";
-const bttn1 = document.createElement("button");
-bttn1.innerHTML = "Start"
-goBttn.appendChild(bttn1);
-mainImg.setAttribute("src", "./images/placeholder-image.png");
+const changeScene = (decision) => {
+    let message = "";
+    let decision1 = "";
+    let decision2 = "";
+    let decision3 = "";
 
-
-bttn1.addEventListener("click", ()=> {
-    let name = prompt("Before we begin, please tell me your name");
-    if (name == "" || name == null){
-        mainText.textContent = "You didnt enter a name, please try again";
-    } else {
-        mainText.textContent = `${name}, let's begin... Hit 'Go' to continue`;
+    switch (currentScene) {
+        case 0:
+            currentScene = 1;
+            message = "It's Monday Morning. You wake up and you have a decision to make. What will you do today?";
+            decision1 = "Go to work";
+            decision2 = "Check the mail";
+            decision3 = "Go out for coffee";
+            document.getElementById(decision2).style.visibility = "visible";
+            document.getElementById(decision3).style.visibility = "visible";
+        break;
+        case 1: 
+            if (decision == 1){
+                currentScene = 2;
+                message = "What a boring day. But, at least work has free coffee. You go home";
+                decision1 = "Start a new day";
+                decision2 = "The end";
+                decision3 = "";
+                document.getElementById(decision3).style.visibility = "hidden";
+            } else if (decision == 2) {
+                currentScene = 2;
+                message = "You have mail! Whoa, it's an invite from a mysterious wizzard. Do you...";
+                decision1 = "Go on an adventure?";
+                decision2 = "Tuen the wizzard down?";
+                decision3 = "";
+                document.getElementById(decision3).style.visibility = "hidden";
+            } else {
+                currentScene = 2;
+                message = "The queue at the coffee place is massive. You decide to...";
+                decision1 = "Go to work";
+                decision2 = "Go home and check the mail";
+                decision3 = "";
+                document.getElementById(decision3).style.visibility = "hidden";
+            }
     }
-const bttn2 = document.createElement("BUTTON");
-bttn2.innerHTML = "Go";
-bttn2.setAttribute("id", "bttn2")
-goBttn.appendChild(bttn2);
+    replaceNodeText("scenetext", message);
+	replaceNodeText("decision1", decision1);
+	replaceNodeText("decision2",decision2);
+	replaceNodeText("decision3",decision3);
 
-})
-
-// second page/choice
-
-// bttn2.addEventListener("click", ()=> {
-//     headerText.textContent = "Day1"
-//     mainText.textContent = `You wake up. It's Monday morning. ${name}, what do you want to do today? Click choose to see your choices.`
-//     const bttn3 = document.createElement("button");
-// bttn3.innerHTML = "Choose"
-// goBttn.appendChild(bttn3);
-// })
-
-// bttn3.addEventListener("click", () => {
-//     let choice1 = prompt("Type the 'choice' in the box Do you want to go to survive the 'earthquake', check the 'mail' or 'mind' your own business")
-// })
-
-    
-
-
-// if (begin === true) {
-//     
-//     
-// }
+}
